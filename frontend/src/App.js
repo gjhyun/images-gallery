82b90a445d48e0d364f0997e27d9914ced5e945f
gjhyun <ginaa.hyun@gmail.com>
2023-09-08 15:27:11 -0400

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState(''); {/** word is part of state of this component. initial value is empty string */}
@@ -10,8 +11,16 @@ function App() {
   const handleSearchSubmit = (e) => { {/** e is an event */}
     e.preventDefault()
     console.log(word)
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
-  
+
   return (
     <div className="App">
       <Header title="Images Gallery"/>
